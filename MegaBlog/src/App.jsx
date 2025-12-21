aa24e895ca8b9db7652f93201939b5478829dd3d
MuhammadAbdullah149 <mabdullah0391@gmail.com>
2025-12-21 18:29:20 +0500

    auth added

--- MegaBlog/src/App.jsx
+++ MegaBlog/src/App.jsx
@@ -3,9 +3,11 @@ import { useState } from 'react'
 
 function App() {
   const [count, setCount] = useState(0)
+  console.log(import.meta.env.VIRE_APPWIRTE_URL);
 
   return (
     <>
+    <h2>Abdullah</h2>
     </>
   )
 }
---
+++ MegaBlog/src/conf/conf.js
@@ -0,0 +1,9 @@
+ const conf = {
+   appwriteURL: String(import.meta.env.VIRE_APPWIRTE_URL),
+   appwriteProjectId: String(import.meta.env.VIRE_APPWIRTE_PROJECT_ID),
+   appwriteDataBaseId: String(import.meta.env.VIRE_APPWIRTE_DATABASE_ID),
+   appwriteCollectionId: String(import.meta.env.VIRE_APPWIRTE_COLLECTION_ID),
+   appwriteBucketId: String(import.meta.env.VIRE_APPWIRTE_BUCKET_ID),
+ };
+
+ export default conf ;
