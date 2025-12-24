82032b41b1c6ec6390a52bbd842997716cb4033b
MuhammadAbdullah149 <mabdullah0391@gmail.com>
2025-12-24 11:09:53 +0500

    input added

---
+++ MegaBlog/src/components/Header/LogoutBtn.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import {useDispatch} from "react-redux";
+import authServices from "../../appwrite-services/auth";
+import {logout} from "../../store/authSlice";        
+
+function LogoutBtn() {
+        const dispatch = useDispatch();
+        const logoutHandler = () =>{
+                authServices.logout()
+                .then(() => {
+                        dispatch(logout());
+                })
+                .catch((error) => {
+                        throw error
+                })
+        }
+  return (
+    <button className="inline-block px-6 py-2 duration-150 rounded-full hover:bg-blue-100" onClick={logoutHandler}
+    >Logout</button>
+  )
+}
+
+export default LogoutBtn
