9d198f554c659a9522429ec3b0e721451bc78757
MuhammadAbdullah149 <mabdullah0391@gmail.com>
2025-12-25 11:50:33 +0500

    added

--- MegaBlog/src/components/index.js
+++ MegaBlog/src/components/index.js
@@ -3,10 +3,13 @@ import Container from "./Container/Container.jsx";
 import Footer from "./Footer/Footer.jsx";
 import Header from './Header/Header.jsx';
 import Input from './Input/Input.jsx';
+import PostForm from './PostForm/PostForm.jsx'
+import Protected from './AuthLayout.jsx'
 import Login from './Login.jsx';
 import Logo from "./Logo.jsx";
 import Postcard from './PostCard.jsx'
+import RTE from './RTE.jsx';
 import Select from './Select.jsx'
 import LogoutBtn from './Header/LogoutBtn.jsx';
 
-export {Button , Container , Header, Footer , Input , Login , Logo , Postcard , Select , LogoutBtn};
+export {Button , Container , Header, Footer , Input , PostForm , Protected , Login , Logo , Postcard , RTE , Select , LogoutBtn};
