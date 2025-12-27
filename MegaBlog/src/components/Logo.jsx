4e083299d1c618c2ba1addf560f247bd4e9ecc3a
MuhammadAbdullah149 <mabdullah0391@gmail.com>
2025-12-27 21:47:14 +0500

    new error handeled

--- MegaBlog/src/components/Logo.jsx
+++ MegaBlog/src/components/Logo.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Logo({width = '100px'}) {
   return (
     <>
-    <img src="https://cdn.logojoy.com/wp-content/uploads/2018/05/30164225/572-768x591.png" height="60px" width="60px" className="rounded-sm" alt="" />
+      <img src="https://digitallychee.com/wp-content/uploads/2021/02/Tripadvisor-Favicon-for-Digital-Lychee-Favicon-Blog-Green-Circle-with-Black-Outlined-Owl-Eyes.png" height="60px" width="60px" className="rounded-4xl" alt="" />
     </>
   )
 }
--- MegaBlog/src/components/RTE.jsx
+++ MegaBlog/src/components/RTE.jsx
@@ -44,7 +44,7 @@ export default function RTE({ name, lable, control, defaultValue = "" }) {
               content_style:
                 "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
             }}
-        
+            onEditorChange={onChange}
           />
         )}
       />
